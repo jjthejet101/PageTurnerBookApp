4f77f7d8cffcc490c1339df943b32b78505bb26b
Justin Jetton <justinkjetton@gmail.com>
2018-06-29 16:34:04 -0500

    Updated cover page
    changed HelloIonicPage to CoverPage and hello-ionic to cover-page

---
+++ BookApp/src/pages/cover-page/cover-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'page-cover-page',
+  templateUrl: 'cover-page.html'
+})
+export class CoverPage {
+  constructor() {
+
+  }
+}
